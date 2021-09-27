5636f7c12cd4ee95b931dc4eeea782e45b09c364
nks102000 <nitishks1110@gmail.com>
2021-09-28 00:43:18 +0530

    Message save in database done

--- app/src/main/java/com/nks/chapappappstone/MessageActivity.java
+++ app/src/main/java/com/nks/chapappappstone/MessageActivity.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.google.firebase.auth.FirebaseAuth;
@@ -20,6 +21,10 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.nks.chapappappstone.Model.User;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -58,6 +63,21 @@ public class MessageActivity extends AppCompatActivity {
         intent = getIntent();
         String  userid = intent.getStringExtra("userid");
         fuser = FirebaseAuth.getInstance().getCurrentUser();
+        btn_send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String msg = text_send.getText().toString();
+                SimpleDateFormat sdf = new SimpleDateFormat("yyyy.MM.dd G 'at' HH:mm:ss z");
+                String currentDateandTime = sdf.format(new Date());
+                if (!msg.equals("")){
+                    sendMessage(fuser.getEmail(), userid, msg,currentDateandTime);
+                } else {
+                    Toast.makeText(MessageActivity.this, "You can't send empty message", Toast.LENGTH_SHORT).show();
+                }
+                text_send.setText("");
+            }
+        });
+
 
         reference = FirebaseDatabase.getInstance().getReference("Users").child(userid);
         reference.addValueEventListener(new ValueEventListener() {
@@ -80,5 +100,60 @@ public class MessageActivity extends AppCompatActivity {
 
             }
         });
+    }
+    private void sendMessage(String sender, final String receiver, String message,String time){
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+
+        HashMap<String, Object> hashMap = new HashMap<>();
+        hashMap.put("message", message);
+        hashMap.put("sender", sender);
+        hashMap.put("timeStamp",time);
+
+        reference.child("Chats").push().setValue(hashMap);
+
+//
+//        // add user to chat fragment
+//        final DatabaseReference chatRef = FirebaseDatabase.getInstance().getReference("Chatlist")
+//                .child(fuser.getUid())
+//                .child(userid);
+//
+//        chatRef.addListenerForSingleValueEvent(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+//                if (!dataSnapshot.exists()){
+//                    chatRef.child("id").setValue(userid);
+//                }
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError databaseError) {
+//
+//            }
+//        });
+//
+//        final DatabaseReference chatRefReceiver = FirebaseDatabase.getInstance().getReference("Chatlist")
+//                .child(userid)
+//                .child(fuser.getUid());
+//        chatRefReceiver.child("id").setValue(fuser.getUid());
+//
+//        final String msg = message;
+//
+//        reference = FirebaseDatabase.getInstance().getReference("Users").child(fuser.getUid());
+//        reference.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+//                User user = dataSnapshot.getValue(User.class);
+//                if (notify) {
+//                    sendNotifiaction(receiver, user.getUsername(), msg);
+//                }
+//                notify = false;
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError databaseError) {
+//
+//            }
+//        });
     }
 }
